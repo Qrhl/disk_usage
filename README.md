3b0cb86587b9c9ec67840a4fefe172f86e94f8c7
Qrhl <quentin.ruhl27@gmail.com>
2018-07-20 14:28:37 +0800

    Add disk_usage.py & update readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+
+
 # disk_usage
+ Checks the disk usage of each partition and sends an email if the threshold is attained.
+ 
+ You need to replace thos values in the script:
+ ```python
+fromaddr = 'sender@test.com'
+rcptaddr = 'recipient@test.com'
+passwd = 'yourpassword'
+smtp_server = 'your_smtp_server'
+```
--- disk_usage.py
+++ disk_usage.py
@@ -4,22 +4,37 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
+threshold = 80
 mount_usage = {}
 du = os.popen("df -h").readlines()
+fromaddr = 'sender@test.com'
+rcptaddr = 'recipient@test.com'
+passwd = 'yourpassword'
+smtp_server = 'your_smtp_server'
+
 
 for line in du[1:]:
-     mount_usage[line.split()[8]] = line.split()[7]
-print(mount_usage)
-over_80 = []
+     mount_usage[line.split()[-1]] = line.split()[-2]
+over_thresh = []
 for disk in mount_usage:
     percent = int(mount_usage[disk].replace("%", ""))
-    print(percent)
-    if percent > 80:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
+    if percent >= threshold:
+        over_thresh.append(disk)
+
+if over_thresh:
+        server = smtplib.SMTP(smtp_server, 587)
+        server.connect(smtp_server, 587)
         server.starttls()
+        server.ehlo()
+        server.login(fromaddr, passwd)
         msg = MIMEMultipart()
-        msg['From'] = 'sender @ gmail.com'
-        msg['To'] = 'receiver @ gmail.com'
-        msg['Subject'] = 'Disk usage above 80%'
-        message = ""
-        msg.attach(MIMEText(message))
+        msg['From'] = fromaddr
+        msg['To'] = rcptaddr
+        msg['Subject'] = 'Disk usage above {}%'.format(threshold)
+        message = "The following disks are above the threshold:\n"
+        for disk in over_thresh:
+            message += " - {} : {}\n".format(disk, mount_usage[disk])
+        msg.attach(MIMEText(message, 'plain'))
+        text = msg.as_string()
+        server.sendmail(fromaddr, rcptaddr, text)
+        server.quit()
